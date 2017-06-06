519bf176ad6a48913c6ed8b6e8c7670276778c97
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-06-05 18:22:18 -0600

    Add hotreads increase count on link

--- spec/requests/links_api_can_create_a_link_spec.rb
+++ spec/requests/links_api_can_create_a_link_spec.rb
@@ -13,8 +13,16 @@ describe 'Links API' do
       expect(link_json['count']).to eq(1)
     end
 
-    xit 'increases the count for an existing link' do
+    it 'increases the count for an existing link' do
+      Link.create!(url: 'http://github.com', count: 1)
+      post '/api/v1/links', params: {url: 'http://github.com'}
 
+      expect(Link.count).to eq(1)
+
+      expect(response).to be_success
+      link_json = JSON.parse(response.body)
+      expect(link_json['url']).to eq('http://github.com')
+      expect(link_json['count']).to eq(2)
     end
   end
 end
